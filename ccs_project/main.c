559746840b9f76aae9762653a02ae0d85c0ad950
José Luiz <joseluizgnogueira@live.com>
2018-04-21 15:37:28 -0300

    msp ports

--- ccs_project/main.c
+++ ccs_project/main.c
@@ -7,17 +7,29 @@ void loop();
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;	// para watchdog timer
 	
 	setup();                    // Configuracoes, (realizado somente na inicializacao da placa)
 
-	while(1) loop();                     // Loop principal
+	while(1){
+	    loop();                     // Loop principal
+	}
 }
 
 void setup(){
+                                // Configura porta P2.1 (Switch 1)
+    SET_PIN(P2,BIT1,INPUT_PULL_UP);
 
+                                // Configura porta P4.7 (LED verde)
+    SET_PIN(P4,BIT7,OUTPUT);
+    WRITE_PIN(P4,BIT7,LOW);
 }
 
-void loop(){
 
+void loop(){
+    if(READ_PIN(P2,BIT1) != 0){     // Se P2.1 != 0 -> true (aberto)
+       WRITE_PIN(P4,BIT7,LOW);        // Desativa P4.7
+    }else{                      // Se P2.1 == 0 -> false (fechado)
+       WRITE_PIN(P4,BIT7,HIGH);           // Ativa P4.7
+    }
 }
--- ccs_project/msp_ports.h
+++ ccs_project/msp_ports.h
@@ -1,3 +1,5 @@
+#include <msp430.h>
+
 /*
  * msp_ports.h
  *
@@ -8,6 +10,22 @@
 #ifndef MSP_PORTS_H_
 #define MSP_PORTS_H_
 
+//----------------VALORES DE SAIDA----------------
+#define HIGH            |=                  // Auxilia macro de saida para 1
+#define LOW             &= ~                // auxilia macro de saida para 0
+
+//------------SETS DAS PORTAS---------------------
+#define SET_OUTPUT(port,pin)              (port##DIR |= pin)                                              // Seta pino como saida
+#define SET_INPUT(port,pin)               (port##DIR &= ~(pin))                                           // Seta pino como entrada
+#define SET_INPUT_PULL_UP(port, pin)      SET_INPUT(port, pin); (port##REN |= pin); (port##OUT |= pin)    // Seta pin = input + pull-up
+#define SET_INPUT_PULL_DOWN(port, pin)    SET_INPUT(port, pin); (port##REN |= pin); (port##OUT &= ~(pin)) // Seta pin = input + pull-down
+
+//-------MACRO DE MANIPULACAO DAS GPIOs----------
+// mode = (OUTPUT; INPUT; INPUT_PULL_UP; INPUT_PULL_DOWN)
+#define SET_PIN(port,pin,mode)      SET_##mode(port,pin)    // Seta o pino com o modo escolhido
+#define WRITE_PIN(port,pin,value)   (port##OUT value pin)   // Escreve a saida no pino no valor (High ou low)
+#define READ_PIN(port,pin)          (port##IN & pin)        // Le o valor no registro de saida do pino
+#define TOOGLE_PIN(port,pin)        (port##OUT ^= pin)      // Alterna valor do pino escolhido
 
 
 #endif /* MSP_PORTS_H_ */
