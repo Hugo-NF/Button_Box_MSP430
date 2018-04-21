6d672697e8dceceb0662313f7e1559fe9fbbaf0e
Hugo Fonseca <hugonfonseca@hotmail.com>
2018-04-21 17:08:25 -0300

    Macros de registro e timers

--- ccs_project/msp_ports.h
+++ ccs_project/msp_ports.h
@@ -20,12 +20,18 @@
 #define SET_INPUT_PULL_UP(port, pin)      SET_INPUT(port, pin); (port##REN |= pin); (port##OUT |= pin)    // Seta pin = input + pull-up
 #define SET_INPUT_PULL_DOWN(port, pin)    SET_INPUT(port, pin); (port##REN |= pin); (port##OUT &= ~(pin)) // Seta pin = input + pull-down
 
-//-------MACRO DE MANIPULACAO DAS GPIOs----------
+//-------MACRO DE MANIPULACAO DAS GPIOs-----------
 // mode = (OUTPUT; INPUT; INPUT_PULL_UP; INPUT_PULL_DOWN)
 #define SET_PIN(port,pin,mode)      SET_##mode(port,pin)    // Seta o pino com o modo escolhido
 #define WRITE_PIN(port,pin,value)   (port##OUT value pin)   // Escreve a saida no pino no valor (High ou low)
 #define READ_PIN(port,pin)          (port##IN & pin)        // Le o valor no registro de saida do pino
 #define TOOGLE_PIN(port,pin)        (port##OUT ^= pin)      // Alterna valor do pino escolhido
+
+
+//-------MACROS PARA MANIPULAÇÂO DE REGISTRO------
+#define SET_REGISTER(reg, bit)      reg |= bit
+#define CLEAR_REGISTER(reg, bit)    reg &= ~(bit)
+#define TOOGLE_REGISTER(reg, bit)   reg ^= bit
 
 
 #endif /* MSP_PORTS_H_ */
---
+++ ccs_project/msp_timers.h
@@ -0,0 +1,13 @@
+/*
+ * msp_timers.h
+ *
+ *  Created on: Apr 21, 2018
+ *      Author: hugonfonseca
+ */
+
+#ifndef MSP_TIMERS_H_
+#define MSP_TIMERS_H_
+
+
+
+#endif /* MSP_TIMERS_H_ */
