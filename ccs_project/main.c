faca2b117bb4e3313d20dccc599229db6a3aa2bf
José Luiz <joseluizgnogueira@live.com>
2018-04-22 15:56:36 -0300

    Leitura dos botões

--- ccs_project/main.c
+++ ccs_project/main.c
@@ -1,9 +1,12 @@
 #include <msp430.h> 
-
-#include "msp_ports.h"
+#include <stdbool.h>
+#include "pins.h"
+#include "msp_timers.h"
 
 void setup();
 void loop();
+
+void handlerButton(unsigned char n_button, unsigned char tmp);
 
 int main(void)
 {
@@ -12,24 +15,70 @@ int main(void)
 	setup();                    // Configuracoes, (realizado somente na inicializacao da placa)
 
 	while(1){
-	    loop();                     // Loop principal
+	    loop();                 // Loop principal
 	}
 }
 
-void setup(){
-                                // Configura porta P2.1 (Switch 1)
-    SET_PIN(P2,BIT1,INPUT_PULL_UP);
+unsigned char buttons_addr[12]={
+    P2_3,   /* B_4 */ //(SB_1)
+    P8_1,   /* B_6 */
+    P2_5,   /* B_5 */ //(SB_3)
+    P2_4,   /* B_3 */ //(SA_3)
+    P1_5,   /* B_2 */ //(SA_1)
+    P1_4,   /* B_1 */
+    P1_3,   /* B_7 */
+    P1_2,   /* B_8 */
+    P4_3,   /* B_9 */
+    P4_0,   /* B_10 */
+    P3_7,   /* B_11 */
+    P8_2    /* B_12 */
+            /* B_12 */ //(SA_2)
+            /* B_14 */ //(SB_2)
+};
 
-                                // Configura porta P4.7 (LED verde)
-    SET_PIN(P4,BIT7,OUTPUT);
-    WRITE_PIN(P4,BIT7,LOW);
+unsigned char buttons_old[14], buttons_new[14];
+
+void setup(){
+    // Configura porta P1.0 (LED vermelho) - SINAL DE LIGADO
+    setPin(P1_0,OUTPUT);
+    writePin(P1_0, LOW);
+
+    // Configura porta p4.7 (LED verde) - SINAL p/ BOTOES
+    setPin(P4_7, OUTPUT);
+    writePin(P4_7, LOW);
+
+    unsigned char i=12;
+    while(i--){
+        setPin(buttons_addr[i], INPUT_PULL_DOWN);
+    }
 }
 
-
 void loop(){
-    if(READ_PIN(P2,BIT1) != 0){     // Se P2.1 != 0 -> true (aberto)
-       WRITE_PIN(P4,BIT7,LOW);        // Desativa P4.7
-    }else{                      // Se P2.1 == 0 -> false (fechado)
-       WRITE_PIN(P4,BIT7,HIGH);           // Ativa P4.7
+    writePin(P1_0, HIGH);           // Ativa led de ligado
+
+    // Leitura das entradas dos pinos
+    unsigned char i=12;
+    while(i--){
+        buttons_new[i] = readPin(buttons_addr[i]);
+    }
+    // Inferencia do switch no estado de desligado
+    buttons_new[12] = !buttons_new[3] && !buttons_new[4];  // SA_1 = 0 e SA_3=0 -> SA_2=1
+    buttons_new[13] = !buttons_new[0] && !buttons_new[2];  // SB_1 = 0 e SB_3=0 -> SB_2=1
+
+    // Lida com os sinais sinais dos botoes
+    i=14;
+    while(i--){
+        handlerButton(i,100);
+    }
+
+}
+
+void handlerButton(unsigned char n_button, unsigned char tmp){
+    if(buttons_old[n_button] != buttons_new[n_button]){
+        if(buttons_new[n_button]){
+            tooglePin(P4_7);
+        }
+        buttons_old[n_button] = buttons_new[n_button];
+        debounce(tmp);
     }
 }
--- ccs_project/msp_ports2.h
+++ ccs_project/msp_ports2.h
@@ -10,6 +10,27 @@
 #ifndef MSP_PORTS_H_
 #define MSP_PORTS_H_
 
+#define B1      P2,BIT3
+#define B2      P8,BIT1
+#define B3      P2,BIT5
+#define B4      P2,BIT4
+#define B5      P1,BIT5
+#define B6      P1,BIT4
+#define B7      P1,BIT3
+#define B8      P1,BIT2
+#define SA1     P4,BIT3
+#define SA2     P4,BIT0
+#define SB1     P3,BIT7
+#define SB2     P8,BIT2
+#define LED_ON_OFF  P1,BIT0
+//#define LED_IN_OUT
+
+//-------MACROS PARA MANIPULAÇÂO DE REGISTRO------
+#define SET_REGISTER(reg, bit)      (reg |= bit)
+#define CLEAR_REGISTER(reg, bit)    (reg &= ~(bit))
+#define TOOGLE_REGISTER(reg, bit)   (reg ^= bit)
+#define READ_REGISTER(reg, bit)     (reg & bit)
+
 //----------------VALORES DE SAIDA----------------
 #define HIGH            |=                  // Auxilia macro de saida para 1
 #define LOW             &= ~                // auxilia macro de saida para 0
@@ -27,11 +48,25 @@
 #define READ_PIN(port,pin)          (port##IN & pin)        // Le o valor no registro de saida do pino
 #define TOOGLE_PIN(port,pin)        (port##OUT ^= pin)      // Alterna valor do pino escolhido
 
+//------------------DEBOUNCE---------------------
+inline void debounce(unsigned short int t_mili){
+    //Seta timer: Clock = ACLK dividido por 9, modo de subida.
+    TA1CTL = (TASSEL__ACLK | MC__UP | ID__8);
 
-//-------MACROS PARA MANIPULAÇÂO DE REGISTRO------
-#define SET_REGISTER(reg, bit)      reg |= bit
-#define CLEAR_REGISTER(reg, bit)    reg &= ~(bit)
-#define TOOGLE_REGISTER(reg, bit)   reg ^= bit
+    //Divide o valor do clock por 4
+    TA1EX0 = TAIDEX_4;
 
+    //Seta modo de saida para SET
+    TA1CCTL0 = OUTMOD_1;
+
+    //Limiar da contagem
+    TA1CCR0 = t_mili;
+
+    // Reseta o timer A1 e inicia a contagem
+    SET_REGISTER(TA1CTL, TACLR);
+
+    // Enquanto o tempo for menor aguarda o delay
+    while(!READ_REGISTER(TA1CCTL0,CCIFG));
+}
 
 #endif /* MSP_PORTS_H_ */
